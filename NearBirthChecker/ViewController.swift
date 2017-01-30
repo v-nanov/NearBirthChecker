10a7bd4ce0d111a364a586a8fae932947b4abaee
최광훈 <hoonhoon@woowahan.com>
2017-01-30 23:29:16 +0900

    Initial Commit

---
+++ NearBirthChecker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NearBirthChecker
+//
+//  Created by hoonhoon on 2017. 1. 30..
+//  Copyright © 2017년 hoonhoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
