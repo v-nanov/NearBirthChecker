9104949a61f90ed9504c5cd9074fa83041ad4d23
최광훈 <hoonhoon@woowahan.com>
2017-01-31 00:12:09 +0900

    first commit

---
+++ NearBirthChecker/Time.swift
@@ -0,0 +1,15 @@
+//
+//  Time.swift
+//  NearBirthChecker
+//
+//  Created by hoonhoon on 2017. 1. 30..
+//  Copyright © 2017년 hoonhoon. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Time: Object {
+    dynamic var start: NSDate?
+    dynamic var end: NSDate?
+}
--- NearBirthChecker/ViewController.swift
+++ NearBirthChecker/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    @IBOutlet weak var startStopButton: UIBarButtonItem!
+    var timesToken: NotificationToken?
+    var times: Results<Time>?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        guard let realm = try? Realm() else { return }
+        self.times = realm.objects(Time.self).sorted(byKeyPath: "start")
+        self.timesToken = self.times?.addNotificationBlock({ (change) in
+            self.tableView.reloadData()
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0 < (self.times?.count ?? 0) ? 1 : 0
+    }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.times?.count ?? 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TimeCell", for: indexPath)
+        if let timeCell = cell as? TimeCell {
+            timeCell.time = self.times?[indexPath.row]
+        }
+        return cell
+    }
+
+    @IBAction func start(_ sender: Any) {
+        guard let realm = try? Realm() else { return }
+        let time = Time()
+        time.start = NSDate()
+        try? realm.write {
+            realm.add(time)
+        }
+    }
+
+    @IBAction func end(_ sender: Any) {
+        guard let realm = try? Realm() else { return }
+        guard let lastTime: Time = times?.last else { return }
+        try? realm.write {
+            lastTime.end = NSDate()
+        }
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+        guard let realm = try? Realm() else { return }
+        try? realm.write {
+            [weak self] in
+            guard (self?.times?.count ?? 0) > 0, let times = self?.times else { return }
+            realm.delete(times)
+        }
+    }
+    
 }
 
